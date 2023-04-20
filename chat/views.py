5766036d48335b39dde9ddb4e2ce410a972f46b8
MCK <mck0819@gmail.com>
2023-04-20 16:12:03 +0900

    오타 및 모델 수정

--- chat/views.py
+++ chat/views.py
@@ -9,29 +9,32 @@ import requests
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def generate_response(messages):
-    url = 'https://api.openai.com/v1/completions'
+    print(messages)
+    url = 'https://api.openai.com/v1/chat/completions'
     headers = {
         'Content-Type': 'application/json',
         'Authorization': f'Bearer {openai.api_key}',
     }
     data = {
-        'model': "text-davinci-003",
-        'messages': messages,
-        'max_tokens': 50,
-        'temperature': 0.7,
+        'model': "gpt-3.5-turbo",
+        'messages': [{"role":"user", "content": messages}],
+
+        'max_tokens': 500,
+        'temperature': 0,
+        #'stream':False,
+        #'logprobs': None,
         'top_p':1,
-        'n': 1,
-        'stop': '\n'
+
     }
     response = requests.post(url, headers=headers, json=data)
     print(response.json())
-    return response.json()['choices'][0]['text'].strip()
-
+    return response.json()['choices'][0]['message']['content'].strip()
+    #return response.json()['error']['message'].strip()
 
 def chatbot_response(request):
     if request.method == 'GET':
         # GET 요청에 대한 처리
-        prompt = request.GET.get('prompt', '')
+        prompt = request.GET.get('message')
         response_text = generate_response(prompt)
         return JsonResponse({'response': response_text})
     else:
